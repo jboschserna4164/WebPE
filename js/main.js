f0bd92fb5a06928f7d2358598ff257027f6dff9e
jboschserna4164 <62222010+jboschserna4164@users.noreply.github.com>
2023-01-26 17:31:44 -0500

    segundo commit
    se hacen las section con sus debidas etiquetas y se le agregan los botones para que los diriga a la seccion deseada

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 window.onload = init; //cargar la funcion init en la ventana
 
 function init(){
-	alert("YO SOY LO MAS FRESA DEL PEDAZO CUAL ES ESA, Y AHORA YO SOY TU GATO BELLACO Y TE VA A COMEL");
+	alert("Bienvenido");
 }
